e8408ce7d4d25a9a6307107023b33be20e992ee1
rreptile <wintermarrkel@gmail.com>
2014-04-12 20:04:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express'),
-  	app = express(),
-	http = require('http'),
-	server = http.createServer(app),
-	io = require('socket.io').listen(server);
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
 
 var config = require('./config')(app, express);
