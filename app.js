a5a6d6961919d9b49ab62d7b1612a75c5a199ba4
Markel Tuzinskiy <wintermarrkel@gmail.com>
2014-04-14 11:54:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,34 @@ var express = require('express'),
 
 var config = require('./config')(app, express);
 
+var squares = [];
+
 io.sockets.on('connection', function(socket){
-    console.log('Client сonnected!');
+    console.log('Client сonnected! Synchronizing squares...');
+    socket.emit('syncSquares', squares);
     
     socket.on('addSquare', function(data){
-    	socket.broadcast.emit('addSquare', data);
+        socket.broadcast.emit('addSquare', data);
+
+        squares.push({x:data.x, y:data.y, color:data.color, id:data.id});
     	console.log('Client added square #'+data.id);
     });
     socket.on('moveSquare', function(data){
-    	socket.broadcast.emit('moveSquare', data);
+        socket.broadcast.emit('moveSquare', data);
+
+        for(var i in squares){
+            var square = squares[i];
+            if(square.id == data.id){square.x = data.x; square.y = data.y;}
+        }
     	console.log('Client moved square #'+data.id);
     });
     socket.on('changeColor', function(data){
     	socket.broadcast.emit('changeColor', data);
+
+        for(var i in squares){
+            var square = squares[i];
+            if(square.id == data.id)square.color = data.color;
+        }
     	console.log('Client changed color of square #'+data.id);
     });
     socket.on('disconnect', function(socket){
